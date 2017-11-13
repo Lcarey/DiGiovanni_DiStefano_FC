3db6d527327d7490088078496cc8526ba52ed8c3
Lucas Carey <lucas.carey@upf.edu>
2017-11-13 07:22:53 +0100

    initiate

---
+++ IDDeletedORFsFromAnnotation.m
@@ -0,0 +1,32 @@
+%% parse strain_annotation_regions.tab and id ORFs that are deleted
+SGD = loadSGDFeaturesDS();
+SGD = dataset2table( SGD( strcmp(SGD.TYPE,'ORF'),:) ) ;
+SGD = SGD( : , {'ORF' 'GENE' 'Chr' 'Start' 'End'} );
+SGD.max_pos = max( [SGD.Start SGD.End], [] , 2) ; 
+SGD.min_pos = min( [SGD.Start SGD.End], [] , 2) ; 
+A = readtable('strain_annotation_regions.tab','FileType','text','Delimiter','\t');
+
+%% fill in genes
+for I = 1:height(SGD)
+    if isempty(SGD.GENE{I})
+        SGD.GENE{I} = SGD.ORF{I};
+    end
+end
+%% pick out ORFs that are deleted in each strain
+A.deleted_orfs = cell( height(A), 1);
+A.deleted_genes = cell( height(A), 1);
+for I = 2:height(A)
+   deleted_regions = regexp( A.regions_deleted{I} , ',' ,'split');
+   for dr = deleted_regions
+       chr = regexprep( char(dr) , ':.*','') ;
+       first_base_del =  str2double( regexprep( regexprep( dr , '.*:','') , '-.*',''));
+       last_base_del  =  str2double( regexprep( dr , '.*-','')); 
+       idx = find(strcmp(SGD.Chr,chr) & (...
+           (SGD.min_pos >= first_base_del & SGD.min_pos <= last_base_del) | ...
+           (SGD.max_pos >= first_base_del & SGD.max_pos <= last_base_del) ) ) ; 
+       A.deleted_orfs{I} = vertcat( A.deleted_orfs{I} , SGD.ORF(idx) );
+       A.deleted_genes{I} = vertcat( A.deleted_genes{I} , SGD.GENE(idx) );
+   end
+end
+
+%%

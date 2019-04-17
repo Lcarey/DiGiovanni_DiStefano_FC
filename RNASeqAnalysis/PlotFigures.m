7917d717c237cbb05b21ac0f669495a917415e6d
Lucas Carey <lucas.carey@gmail.com>
2019-04-17 14:51:06 +0800

    a

--- RNASeqAnalysis/PlotFigures.m
+++ RNASeqAnalysis/PlotFigures.m
@@ -3,19 +3,32 @@ ESR_UP_list = BB.ORF( strcmp(BB.ESR,'up'));
 ESR_DN_list = BB.ORF( strcmp(BB.ESR,'down'));
 GR_UP_list = BB.ORF(strcmp(BB.GrowthRateResponse_1_5SD,'up'));
 GR_DN_list = BB.ORF(strcmp(BB.GrowthRateResponse_1_5SD,'down'));
-
+%%
+cd('~/Develop/DiGiovanni_DiStefano_FC/RNASeqAnalysis/');
 load('PP.mat');
+SGD = dataset2table( loadSGDFeatures() );
 %high_expressed_idx =  PP.Expr_409 > prctile(PP.Expr_409 , 50) ; 
 %PP = PP( high_expressed_idx , :) ; 
 % Plot some figures
 figname = '~/Downloads/FoldChange_vs_Pdiff.eps';
 
+%% calculate the distance to the centromere for each gene
+SGD.KB2CEN = NaN( height(SGD),1);
+for I = 1:height(SGD)
+    cen_pos = SGD.Start( strcmp(SGD.Chr , SGD.Chr{I})  & strcmp(SGD.TYPE,'centromere'));
+    if ~isempty( cen_pos )
+        SGD.KB2CEN(I) = abs(SGD.Start(I) - cen_pos) ./ 1000 ;
+    end
+end
+SGD = SGD( strcmp(SGD.TYPE,'ORF'),:);
+PP = innerjoin( SGD( :  ,  {'ORF' 'KB2CEN'}) , PP , 'LeftKey','ORF','RightKey','target_id' ); 
 %% Arms with deletions
 
     
 X = NaN(0);
 Y = NaN(0);
 dt = NaN(0);
+dCEN = NaN(0);
 orfs = cell(0);
 ID = NaN(0);
 idx_on_arm_with_deletion = NaN(0);
@@ -25,14 +38,15 @@ for I = 2:height(A)
     vnP =  [ 'Pdiff_' num2str(A.ID(I)) ] ;
     expr = PP.(vnE) ; 
     expr_in_409 = vertcat( expr_in_409 , PP.Expr_409 ) ;
-    gene_chr_arm_ids = cellfun( @(X)X(1:3) , PP.target_id ,'UniformOutput',false );
+    gene_chr_arm_ids = cellfun( @(X)X(1:3) , PP.ORF ,'UniformOutput',false );
     these_arms_have_tel_deletions = unique(gene_chr_arm_ids(isnan(expr))) ; 
     idx_on_arm_with_deletion = vertcat( idx_on_arm_with_deletion , ismember(gene_chr_arm_ids,these_arms_have_tel_deletions) ); 
     X = vertcat( X , PP.(vnP));
     Y = vertcat( Y , expr );
     dt = vertcat( dt , PP.nt_to_closest_end ) ; 
+    dCEN = vertcat( dCEN , PP.KB2CEN );
     ID = vertcat( ID , repmat(A.ID(I) , numel(PP.(vnP)),1) );
-    orfs = vertcat( orfs , PP.target_id);
+    orfs = vertcat( orfs , PP.ORF);
 end
 idx_on_arm_with_deletion = logical(idx_on_arm_with_deletion) ; 
 %% group by %PP
@@ -41,8 +55,9 @@ GROUPS = round(X/5)*5 ;
 [ug,n]=count_unique(GROUPS) ;
 keep_groups = ug(n>=10);
 Y = Y( ismember(GROUPS,keep_groups));
+dt = dt(ismember(GROUPS,keep_groups));
+dCEN = dCEN(ismember(GROUPS,keep_groups));
 GROUPS = GROUPS( ismember(GROUPS,keep_groups));
-
 %% optionally, remove genes that are part of the ESR or Growth Rate Response
 DISCARD_FLAG  = false ; 
 
--- RNASeqAnalysis/displacement with respect to the nuclear envelope for loci as a function of distance from the end of the chromosome in wild-type cells.m
+++ RNASeqAnalysis/displacement with respect to the nuclear envelope for loci as a function of distance from the end of the chromosome in wild-type cells.m
@@ -1,9 +1,114 @@
 WORKDIR = '/Users/lcarey/Develop/DiGiovanni_DiStefano_FC/Data/ModelPredictions/';
+fn = 'genomic_distance_from_telomere_vs_displacement_in_FC_strains_wrt_WT_with_chromosome_position.txt'  ;
+% fn = 'genomic_distance_from_closest_chr_arm_end_vs_displacement_in_FC_strains_wrt_WT_with_chromosome_position.txt'  ;
 
-T = readtable( [WORKDIR 'scatter_plot_distance_from_chr_arm_end_vs_displacement_in_FC.txt']);
-%% Figure 5. Loci near the end of fused chromosomes are predicted to be displaced away from the nuclear periphery. (A) The predicted displacement with respect to the nuclear envelope for loci in fused (blue) and non-fused (orange) chromosomes. (B) The predicted displacement with respect to the nuclear envelope for loci as a function of distance from the end of the chromosome in wild-type cells. Loci that are closer to the ends of the chromosomes exhibit a greater change away from the nuclear envelope. 
-figure ; 
-plot(T.Var1./1000,T.Var2,'ok')
+T = readtable( [WORKDIR fn]);
+T = sortrows(T,'DisplacementFromNEwrtWT','descend');
+
+
+T.chr = str2double(regexprep( regexprep(T.Locus,'^chr','') , '_.*','')) ; 
+T.chrROM = num2roman(T.chr);
+
+
+% which are subtelomeric?
+T.subtelomeric50 = T.GenDistToTEL < 50*1000 ;
+T.subtelomeric100 = T.GenDistToTEL < 100*1000 ;
+T.subtelomeric75 = T.GenDistToTEL < 75*1000 ;
+
+%% load CEN positions
+SGD = dataset2table( loadSGDFeatures() );
+SGD = SGD( strcmp(SGD.TYPE,'centromere') , :);
+SGD.Chr = str2double(SGD.Chr);
+
+% identify midpoint of each locus, and dist 2 centromere
+T.chrpos = NaN(height(T),1);
+for I = 1:height(T)
+    metadata = regexp( T.Locus{I} , '_' , 'split');
+    T.chrpos(I) = mean(str2double(metadata(3:4))) ; 
+    T.KB2CEN(I) = abs( T.chrpos(I) - SGD.Start( SGD.Chr==T.chr(I)) ) ./ 1000 ;
+end
+
+% which are peri-centromatic ?
+T.pericen25 = T.KB2CEN < 25 ;
+T.pericen50 = T.KB2CEN < 50 ;
+T.pericen75 = T.KB2CEN < 75 ;
+T.pericen100 = T.KB2CEN < 100 ;
+
+%% we don't really care about Donor vs Recipient. Just Fused vs non-fused
+idx = find( ~strcmp(T.Fused_NoFused,'NoFused')) ; 
+for I = 1:numel(idx)
+    T.Fused_NoFused{idx(I)} = 'Fused';
+end
+
+
+ 
+%%
+    %gscatter(  T.GenDistToTEL./1000 , T.DisplacementFromNEwrtWT , { T.Terminal_InternalTEL T.Fused_NoFused} )
+figbasename = '~/Downloads/Fig5B_' ; 
+T.ID = regexprep( strcat( T.Terminal_InternalTEL, ' and  ' , ' ; ' ,T.Fused_NoFused) , ';' , '') ; 
+uids = unique(T.ID);
+clrs = [ 0 0 1 ; 0 1 0 ;1 0 0]
+for I = 1:numel(uids)
+
+    fh = figure('units','centimeters','position',[5 5 8 8 ]); 
+    hold on ;
+    plot( T.GenDistToTEL./1000 , T.DisplacementFromNEwrtWT , 'o' ,'Color',[.5 .5 .5]  , 'MarkerFaceColor' , [.7 .7 .7] ) ; 
+ 
+    idx = strcmp(T.ID,uids{I});
+    plot( T.GenDistToTEL(idx)./1000 , T.DisplacementFromNEwrtWT(idx) , 'o' ,'Color',[.1 .1 .1] , 'MarkerFaceColor' , clrs(I,:) ) ; 
+    
+%     plot( T.GenDistToTEL(idx & T.pericen75)./1000 , T.DisplacementFromNEwrtWT(idx& T.pericen75) , '.c' ) ; 
+%     plot( T.GenDistToTEL(idx & T.pericen50)./1000 , T.DisplacementFromNEwrtWT(idx& T.pericen50) , '.g' ) ; 
+%     plot( T.GenDistToTEL(idx & T.pericen25)./1000 , T.DisplacementFromNEwrtWT(idx& T.pericen25) , '.r' ) ; 
+
+    
+xlabel('Distance from the telomere (kb)');
+ylabel('Displacement from N.E. (nm)')
 set(gca,'xscale','log')
-xlim([0 600])
+xlim([0 1000])
 ylim([-50 250])
+%title(uids{I});
+print('-dpng', [figbasename num2str(I) '_' uids{I} ] , '-r300');
+close;
+end
+
+    fh = figure('units','centimeters','position',[5 5 14 8 ]); 
+    hold on ;
+ %   idx = regexpcmp(T.ID,'d');  
+    plot( T.GenDistToTEL./1000 , T.DisplacementFromNEwrtWT , 'o','Color',[.5 .5 .5] , 'MarkerFaceColor' , [.7 .7 .7] ) ; 
+ %   plot( T.GenDistToTEL( idx&T.pericen75)./1000 , T.DisplacementFromNEwrtWT(idx&T.pericen75) , '.c' ) ; 
+ %   plot( T.GenDistToTEL( idx&T.pericen50)./1000 , T.DisplacementFromNEwrtWT(idx&T.pericen50) , '.g' ) ; 
+ %   plot( T.GenDistToTEL( idx&T.pericen25)./1000 , T.DisplacementFromNEwrtWT(idx&T.pericen25) , '.r' ) ; 
+    scatter( T.GenDistToTEL(T.pericen100)./1000 , T.DisplacementFromNEwrtWT(T.pericen100) , 15 , T.KB2CEN(T.pericen100) ,'filled')
+    
+xlabel('Distance from the telomere (kb)');
+ylabel('Displacement from N.E. (nm)')
+set(gca,'xscale','log')
+xlim([0 1000])
+ylim([-50 250])
+colorbar('Direction','reverse')
+print('-dpng', [figbasename 'pcen' ] , '-r300');
+close;
+
+
+%%   fix bugs in code that generated the .txt file
+% no longer necessary. file fixed
+% % % fix bug that wrongly assigns fused / not fused
+% % for I = 1:height(T)
+% %     if ~isempty(regexp( T.x_Fusion{I} , ['_' T.chrROM{I} '_'])) %matches
+% %         T.Fused_NoFused{I} = 'Fused' ;
+% %     end
+% % end
+% % 
+% % % $ grep InternalTEL genomic_distance_from_telomere_vs_displacement_in_FC_strains_wrt_WT_with_chromosome_position.txt|grep NoFused|head
+% % % #Fusion Locus Fused/NoFused Arm Telomere Terminal/InternalTEL GenDistToTEL DisplacementFromNEwrtWT
+% % % LC_IV_XV_V_VII_cen4 chr5_1_0_9600 NoFused armL TELL5 InternalTEL 4800 207.85
+% % % LC_IV_XV_V_VII_cen4 chr5_2_9600_19200 NoFused armL TELL5 InternalTEL 14400 189.965
+% % % LC_IV_XV_V_VII_cen4 chr5_3_19200_28800 NoFused armL TELL5 InternalTEL 24000 172.175
+% % 
+% % % can't be InternalTEL & NoFused. Impossible. 
+% % % change all to Donor
+% % idx = find(strcmp(T.Terminal_InternalTEL,'InternalTEL') & strcmp(T.Fused_NoFused,'NoFused')) ; 
+% % for I = 1:numel(idx)
+% %     T.Fused_NoFused{idx(I)} = 'Donor';
+% % end

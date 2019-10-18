62c6f72aa978bbd7cc1af125f46956b5250a6018
Lucas Carey <lcarey@LucasMac.local>
2019-10-18 10:16:26 +0800

    a

---
+++ Experimental_vs_Predicted_InterLocusDistances/measured_interlocus_distances_vs_simulated__for_revisions.m
@@ -0,0 +1,53 @@
+%% load data
+S = readtable('simulations.txt','FileType','text');
+S.X = round(S.X);
+S = sortrows(S,{'X','Y'},'ascend');
+S.whichpoint = categorical( repmat( {'low' 'mean' 'high'}' , 11 , 1) );
+genotypes = upper( {'WT' 'FC(IV:XII)cen4' 'FC(IV:XII)cen12' 'FC(IV:XV)cen4' 'FC(IV:XV)cen15' ...
+    'FC(IV:XV:V)cen4' 'FC(IV:XV:V)cen5' 'FC(IV:XV:XVI)cen4' 'FC(IV:XV:XVI)cen16' 'FC(IV:XV:V:VII)cen4'...
+    'FC(IV:XV:V:VII)cen7'} )  ; 
+
+S.genotypes = categorical( reshape( repmat(genotypes,3,1) , height(S) ,1) ) ;
+S.genotype = cellfun(@(X)regexprep( X,'[:()]','_'),cellstr(S.genotypes) ,'UniformOutput',false) ;
+S.genotype2 = cellstr( S.genotype);
+S = sortrows(S,'genotype2','descend')
+
+%% load experimental data
+E = readtable('Results FC TRP1-LYS4 distances.xlsx');
+E = stack(E,E.Properties.VariableNames) ; 
+E.Properties.VariableNames = {'genotype' 'distance'} ;
+E.genotype = cellfun(@(X)regexprep( X,'.*YMM\d+_',''),cellstr(E.genotype) ,'UniformOutput',false) ;
+G = grpstats( E , 'genotype' , {'mean' 'median' 'std' 'sem'} , 'DataVars','distance');
+G.genotype2 = cellstr( G.genotype);
+G = sortrows(G,'genotype2','descend')
+
+%%
+
+figure; 
+subplot(2,1,1)
+errorbar( 1:11 , S.Y( S.whichpoint=='mean') ,  S.Y( S.whichpoint=='mean')-S.Y( S.whichpoint=='low') , S.Y( S.whichpoint=='high')- S.Y( S.whichpoint=='mean') ,'-ok','MarkerFaceColor',[.7 .7 .7])
+axis tight; 
+title('simulated')
+set(gca,'xtick',1:11);
+set(gca,'xticklabel',regexprep( cellstr( S.genotype(S.whichpoint=='mean')) , '_' ,' ') );
+xlim([0.5 11.5])
+
+subplot(2,1,2)
+errorbar( 1:11 , G.mean_distance , G.sem_distance ,'-ok','MarkerFaceColor',[.7 .7 .7])
+title('experimental')
+set(gca,'xtick',1:11);
+axis tight; 
+set(gca,'xticklabel',regexprep( cellstr(G.genotype) , '_' ,' ') )
+xlim([0.5 11.5])
+
+[c,p] = corr( S.Y(S.whichpoint=='mean') ,  G.mean_distance , 'type','Spearman')
+[c,p] = corr( S.Y(S.whichpoint=='mean') ,  G.mean_distance , 'type','Pearson')
+
+%%
+fh = figure('units','centimeters','position',[5 5 8 8]);
+hold on ;
+errorbar( G.mean_distance ,  S.Y( S.whichpoint=='mean') ,  S.Y( S.whichpoint=='mean')-S.Y( S.whichpoint=='low') , S.Y( S.whichpoint=='high')- S.Y( S.whichpoint=='mean') ,'ok' ,'MarkerFaceColor',[.7 .7 .7]) ;
+herrorbar( G.mean_distance ,  S.Y( S.whichpoint=='mean') , G.sem_distance , G.sem_distance ,'ok') ;
+xlabel('Experimentally measured distance')
+ylabel('Simulated inter-locus distance')
+gscatter( G.mean_distance ,  S.Y( S.whichpoint=='mean') , regexprep( cellstr(G.genotype) , '_' ,' ') , parula(height(G)) ,'.',30)
